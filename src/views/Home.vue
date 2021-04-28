3b601efdea052734cbd9b538e1fddb963e4422c2
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-28 15:13:05 +0900

    Add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<main class="home">
+		<header>
+			<div class="title">
+				<h1>Welcome to FREESERVER</h1>
+			</div>
+			<scroll-to-see-more @click="$router.push({name: 'Home', hash: '#about'})"/>
+		</header>
+		<div id="about">About</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<div id="wiki">Wiki</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+	</main>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ScrollToSeeMore from '@/components/ScrollToSeeMore.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		ScrollToSeeMore
+	}
 }
 </script>
+<style scoped>
+.home {
+	width: 100%;
+}
+
+.home header {
+	height: 100vh;
+	background-size: cover;
+	background-image: url(/assets/pics/mainBackground.png);
+	color: white;
+	position: relative;
+}
+
+.home header .title {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	-webkit-transform: translateY(-50%) translateX(-50%);
+	transform: translateY(-50%) translateX(-50%);
+}
+
+.home header .title h1 {
+	font-weight: 100;
+	display: inline-block;
+}
+
+.home header .scroll-to-see-more {
+	position: absolute;
+	bottom: 10px;
+	left: 50%;
+	-webkit-transform: translateX(-50%);
+	transform: translateX(-50%);
+}
+</style>
