85d688fc6961860a583dab3f882af6ef7250a7c1
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-28 15:14:39 +0900

    Revert "Revert "Add logo""
    This reverts commit 3170cdec15629e47734c0fc3d900acbf4274ca65.

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="wrapper">
+		<f-nav />
+		<router-view />
+	</div>
+</template>
+
+<script>
+import FNav from '@/components/Nav.vue';
+
+export default {
+	name: 'Main',
+	components: { FNav }
+}
+</script>
