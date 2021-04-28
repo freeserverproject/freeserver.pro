dc5d2cc8293f77146bebaaac778ba1bc0864240d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 19:21:21 +0900

    ✨Added new font

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&display=swap');
 
 html {
 	font-size: 62.5%;
@@ -16,7 +17,7 @@ body {
 }
 
 #app {
-	font-family: 'Montserrat', 'Avenir', Helvetica, Arial, sans-serif;
+	font-family: 'Montserrat', 'M PLUS 1p', 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 }
