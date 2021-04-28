0ae127f0fea59f3aaa41c85dc9ac7f3598b06ede
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-28 23:49:30 +0900

    Fix typo

--- src/articles/JoinUs.vue
+++ src/articles/JoinUs.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h1>運営募集について</h1>
-		<a href="https://forms.gle/wA7GcyhXX5jjXxsc9">GoogleFourm</a>にて応募お願いします
+		<a href="https://forms.gle/wA7GcyhXX5jjXxsc9">GoogleForms</a>にて応募お願いします
 	</div>
 </template>
 <script>
