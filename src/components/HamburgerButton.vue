2ae32835ee2777bc13069982b875a3033635c0ad
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-04-29 02:46:33 +0900

    ハンバーガーメニュー実装

---
+++ src/components/HamburgerButton.vue
@@ -0,0 +1,44 @@
+<template>
+	<label class="hamburger-button">
+		<input type="checkbox" @change="$emit('update:modelValue', $event.target.checked)" :checked="modelValue">
+	</label>
+</template>
+<script>
+export default {
+	name: 'HamburgerButton',
+	emits: ['update:modelValue'],
+	props: {
+		modelValue: {
+			type: Boolean,
+			default: false
+		}
+	}
+}
+</script>
+<style scoped>
+label {
+	width: 40px;
+	height: 34px;
+	box-sizing: border-box;
+	z-index: 1000;
+	border-top: 2px solid white;
+	border-bottom: 2px solid white;
+	position: relative;
+}
+
+label::before {
+	background-color: white;
+	content: "";
+	position: absolute;
+	width: 100%;
+	height: 2px;
+	left: 50%;
+	top: 50%;
+	-webkit-transform: translateY(-50%) translateX(-50%);
+	transform: translateY(-50%) translateX(-50%);
+}
+
+input[type="checkbox"] {
+	display: none;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,25 +5,29 @@
 				<img src="../assets/logo.png" alt="FREESERVER Logo">
 			</router-link>
 		</span>
-		<div class="links">
+		<div class="links" :class="{open: drawerOpen}" @click="drawerOpen = false">
 			<hash-match-link :to="{name: 'Home', hash: '#about'}">About</hash-match-link>
 			<hash-match-link :to="{name: 'Home', hash: '#feature'}">Feature</hash-match-link>
 			<hash-match-link :to="{name: 'Home', hash: '#wiki'}">Wiki</hash-match-link>
 			<hash-match-link :to="{name: 'Home', hash: '#sns'}">Socials</hash-match-link>
 			<a href="//maps.freeserver.pro/">Maps</a>
 		</div>
+		<hamburger-button v-model="drawerOpen"/>
 	</nav>
 </template>
 <script>
 import HashMatchLink from './HashMatchLink.vue';
+import HamburgerButton from './HamburgerButton.vue';
 
 export default {
 	name: 'FNav',
 	components: {
-		HashMatchLink
+		HashMatchLink,
+		HamburgerButton
 	},
 	data: () => ({
-		scroll: 0
+		scroll: 0,
+		drawerOpen: false
 	}),
 	props: {
 		zooming: {
@@ -38,7 +42,7 @@ export default {
 	},
 	mounted () {
 		window.addEventListener('scroll', () => {
-			this.scroll = window.scrollY
+			this.scroll = window.scrollY;
 		});
 	}
 }
@@ -50,7 +54,7 @@ export default {
 	box-sizing: border-box;
 	display: flex;
 	background-color: #00000085;
-	padding: 0 100px;
+	padding: 0 80px;
 	backdrop-filter: blur(5px);
 	z-index: 900;
 }
@@ -62,7 +66,7 @@ export default {
 .logo img {
 	height: 65px;
 	transition: height 300ms 0s ease;
-	transform: scale(1.3)
+	transform: scale(1.2)
 }
 
 .global-nav.zoom .logo img {
@@ -72,6 +76,57 @@ export default {
 .global-nav .links {
 	display: flex;
 	font-size: 2rem;
+}
+
+.hamburger-button {
+	display: none;
+}
+
+@media screen and (max-width: 739px) {
+	.global-nav {
+		padding: 0 20px;
+	}
+	.global-nav .links {
+		font-size: 1.5rem;
+	}
+}
+
+
+@media screen and (max-width: 611px) {
+	.global-nav {
+		padding: 0 20px;
+		padding-left: 0;
+	}
+	.global-nav .links {
+		background-color: #00000081;
+		box-sizing: border-box;
+		backdrop-filter: blur(10px);
+		flex-direction: column;
+		position: fixed;
+		right: 0;
+		top: 0;
+		height: 100vh;
+		width: 100%;
+		padding: 30px;
+		padding-top: 80px;
+		right: -100%;
+
+		transition: right 300ms 0s ease;
+	}
+	.global-nav .links.open {
+		right: 0%;
+	}
+
+	.global-nav .links>* {
+		display: block;
+		font-size: 2.5rem;
+	}
+	.hamburger-button {
+		display: initial;
+	}
+	.logo img {
+		transform: scale(1)
+	}
 }
 
 </style>
