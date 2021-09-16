52aaae2e4ee353a26c52a1bf1fa86584d75be809
Nakatai <nakatai0322@gmail.com>
2021-09-16 15:57:31 +0900

    Update JoinUs.vue

--- src/articles/JoinUs.vue
+++ src/articles/JoinUs.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
 		<h1>運営募集について</h1>
-		<bold><a href="https://forms.gle/wA7GcyhXX5jjXxsc9">Googleフォーム</a></bold>にて応募お願いします
+		<bold><a href="https://forms.gle/wA7GcyhXX5jjXxsc9">Googleフォーム</a></bold>にて応募お願いします<br>
+		詳細に関しては<a href="https://wiki.freeserver.pro/recruit-info.html">Wikiの記事</a>を御覧ください。
 	</div>
 </template>
 <script>
