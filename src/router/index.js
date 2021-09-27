4ab29f167bddd3adcd348a46ddef1c313b1cd559
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-09-27 21:13:43 +0900

    OGP設定

--- src/router/index.js
+++ src/router/index.js
@@ -2,35 +2,41 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 
 const router = createRouter({
-	history: createWebHistory(),
-	scrollBehavior(to, from, savedPosition) {
-		if (savedPosition) return savedPosition;
+  history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
 
-		const position = {
-			behavior: 'smooth',
-			top: 90
-		};
+    const position = {
+      behavior: 'smooth',
+      top: 90
+    };
 
-		if (to.hash && document.querySelector(to.hash)) position.el = to.hash;
-		else {
-			position.top = 0;
-			position.left = 0;
-		}
-		return position;
-	},
-	routes
+    if (to.hash && document.querySelector(to.hash)) position.el = to.hash;
+    else {
+      position.top = 0;
+      position.left = 0;
+    }
+    return position;
+  },
+  routes
 });
 
 router.beforeEach((to, from, next) => {
-	const hash = to.path.match(/\/?%23(.*)$/);
-	if (hash && hash[1] && !to.hash) {
-		next({
-			path: to.path.replace(new RegExp(`${hash[0]}$`), ''),
-			hash: `#${hash[1]}`
-		});
-	} else {
-		next();
-	}
+  const hash = to.path.match(/\/?%23(.*)$/);
+  if (hash && hash[1] && !to.hash) {
+    next({
+      path: to.path.replace(new RegExp(`${hash[0]}$`), ''),
+      hash: `#${hash[1]}`
+    });
+  } else {
+    next();
+  }
+});
+
+router.afterEach(to => {
+  // OGP
+  document.querySelector('meta[property="og:url"]').setAttribute('content', to.fullPath);
+  document.querySelector('meta[property="og:title"]').setAttribute('content', to.meta.title ? `${to.meta.title} | FREESERVER` : 'FREESERVER');
 });
 
 export default router;
